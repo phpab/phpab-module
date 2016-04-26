3f1da17c69c5d7f0d6a9a59cc35d7bb538085d4b
Walter Tamboer <walter@tamboer.nl>
2016-04-27 00:23:03 +0200

    Added file header docblocks.

--- src/View/Helper/IsActive.php
+++ src/View/Helper/IsActive.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * This file is part of phpab/phpab-module. (https://github.com/phpab/phpab-module)
+ *
+ * @link https://github.com/phpab/phpab-module for the canonical source repository
+ * @copyright Copyright (c) 2015-2016 phpab. (https://github.com/phpab/)
+ * @license https://raw.githubusercontent.com/phpab/phpab-module/master/LICENSE MIT
+ */
 
 namespace PhpAbModule\View\Helper;
 
