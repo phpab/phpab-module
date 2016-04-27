c634d05fb6c692804584f8af4b48481b81926c41
Walter Tamboer <walter@tamboer.nl>
2016-04-27 22:50:25 +0200

    Added tests for the data collector to be added to the event dispatcher.

--- tests/PhpAbModuleTest/Service/DispatcherFactoryTest.php
+++ tests/PhpAbModuleTest/Service/DispatcherFactoryTest.php
@@ -9,14 +9,16 @@
 
 namespace PhpAbModuleTest\Service;
 
+use PhpAb\Analytics\Google\DataCollector;
 use PhpAb\Event\Dispatcher;
 use PhpAbModule\Service\DispatcherFactory;
 use PHPUnit_Framework_TestCase;
+use stdClass;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
 class DispatcherFactoryTest extends PHPUnit_Framework_TestCase
 {
-    public function testCreateService()
+    public function testWithoutDataCollector()
     {
         // Arrange
         $serviceLocator = $this->getMockForAbstractClass(ServiceLocatorInterface::class);
@@ -27,5 +29,82 @@ class DispatcherFactoryTest extends PHPUnit_Framework_TestCase
 
         // Assert
         $this->assertInstanceOf(Dispatcher::class, $result);
+    }
+
+    public function testWithDataCollector()
+    {
+        // Arrange
+        $serviceLocator = $this->getMockForAbstractClass(ServiceLocatorInterface::class);
+        $serviceLocator
+            ->expects($this->at(0))
+            ->method('get')
+            ->with($this->equalTo('Config'))
+            ->willReturn([
+                'phpab' => [
+                    'analytics' => [
+                        'collector' => 'my-collector',
+                    ],
+                ],
+            ]);
+
+        $serviceLocator
+            ->expects($this->at(1))
+            ->method('has')
+            ->with($this->equalTo('my-collector'))
+            ->willReturn(true);
+
+
+        $serviceLocator
+            ->expects($this->at(2))
+            ->method('get')
+            ->with($this->equalTo('my-collector'))
+            ->willReturn(new DataCollector());
+
+        $service = new DispatcherFactory();
+
+        // Act
+        $result = $service->createService($serviceLocator);
+
+        // Assert
+        $this->assertInstanceOf(Dispatcher::class, $result);
+    }
+
+    /**
+     * @expectedException RuntimeException
+     * @expectedExceptionMessage The data collector is not an instance of PhpAb\Event\SubscriberInterface
+     */
+    public function testWithInvalidDataCollector()
+    {
+        // Arrange
+        $serviceLocator = $this->getMockForAbstractClass(ServiceLocatorInterface::class);
+        $serviceLocator
+            ->expects($this->at(0))
+            ->method('get')
+            ->with($this->equalTo('Config'))
+            ->willReturn([
+                'phpab' => [
+                    'analytics' => [
+                        'collector' => 'my-collector',
+                    ],
+                ],
+            ]);
+
+        $serviceLocator
+            ->expects($this->at(1))
+            ->method('has')
+            ->with($this->equalTo('my-collector'))
+            ->willReturn(true);
+
+
+        $serviceLocator
+            ->expects($this->at(2))
+            ->method('get')
+            ->with($this->equalTo('my-collector'))
+            ->willReturn(new stdClass());
+
+        $service = new DispatcherFactory();
+
+        // Act
+        $result = $service->createService($serviceLocator);
     }
 }
